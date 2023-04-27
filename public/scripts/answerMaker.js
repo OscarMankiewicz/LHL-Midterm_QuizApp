7531af124127ec95869a3a54ce0c72e1c0127c85
William <Williamtviet@hotmail.com>
2023-04-27 16:01:38 -0700

    added a answermaker.js and on /newquiz, someone can add more questions

---
+++ public/scripts/answerMaker.js
@@ -0,0 +1,25 @@
+const addAnswerButton = document.querySelector('.AddAnswer');
+
+addAnswerButton.addEventListener('click', function(event) {
+  event.preventDefault();
+});
+
+$(document).ready(function() {
+  let counter = 2
+  $(".AddAnswer").on("click", function() {
+    counter++
+    var newAnswer = $("<textarea>").attr({
+      "placeholder": "Answer"+counter,
+      "id": "Answer"+counter
+    });
+    $(this).before(newAnswer);
+
+    if (counter >= 4) {
+      $(addAnswerButton).hide();
+    }
+  });
+});
+
+
+
+console.log ("debug check if answermaker works")
