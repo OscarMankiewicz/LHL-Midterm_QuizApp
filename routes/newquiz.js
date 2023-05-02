3734839fb5e10fc1fd6a58076048a36a1043314d
Aws-star <awsalrahim@yahoo.com>
2023-05-01 20:07:11 -0400

    "Update Logo"

--- routes/newquiz.js
+++ routes/newquiz.js
@@ -3,6 +3,7 @@ const router  = express.Router();
 const userQueries = require('../db/queries/users');
 
 app.get('/newquiz', (req, res) => { //relocate later
+  console.log( "Serveeeeer",__dirname)
   res.render('newquiz')
 })
 
