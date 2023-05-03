dba99b81d33b35acaaf777c37beda28922132fdf
William <Williamtviet@hotmail.com>
2023-05-02 23:56:09 -0700

    added a generateUrl fuction in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,12 @@
 // Client facing scripts here
+
+//for use later in making the quizurl
+function generateUrl() {
+  const alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
+  let quizUrl = '';
+  for (let i = 0; i < 7; i++) {
+    const randomUnit = Math.floor(Math.random() * alphabet.length);
+    code += alphabet[randomUnit];
+  }
+  return quizUrl;
+}
