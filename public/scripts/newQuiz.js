40b395bb8319e08bdc6cb7fe3b501fa0deac75d5
Aws-star <awsalrahim@yahoo.com>
2023-05-04 18:42:56 -0400

    All works for answers and questions design

--- public/scripts/newQuiz.js
+++ public/scripts/newQuiz.js
@@ -1,21 +1,10 @@
-const addNewQuiz = document.querySelector('#title-maker');
+const addNewQuiz = document.querySelector('#submit');
 
 $(document).ready(function() {
-  $(".addAnswerButton").on("click", function(event) {
+  $("#submit").on("click", function(event) {
     event.preventDefault();
-
-    if ($("#title-maker").val().length === 0) {
-      $('.error-message').show();
-      $('.error-message').text("The Quiz Topic shouldn't be empty!");
-    } else {
-      $('.error-message').text("");
-      $('.error-message').hide();
-      $.post("/tweets",  $(this).serialize(),   function(response, status)  {
-      // Clear text area
-        $("#title-maker").val("");
-        $(this).before(addNewQuiz);
-      });
-    }
+    console.log("submitting");
+    location.reload();
   });
 });
 
@@ -25,7 +14,7 @@ console.log("If quiz topic works");
 
 
 //ill try and implement this later
-const addProperty = function(property) {
+/*const addProperty = function(property) {
   const query = {
     text: 'INSERT INTO Quiz (UsersID, Questions=1) VALUES ($1, $2,) RETURNING *',
     values: [
@@ -42,4 +31,4 @@ const addProperty = function(property) {
       console.log(err.message);
       throw err;
     });
-};
+};*/
