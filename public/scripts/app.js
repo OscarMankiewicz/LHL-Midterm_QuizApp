05c200051fd8c501236912a7bb8959162588b05f
Oscar <omankiewicz77@gmail.com>
2023-05-04 22:51:44 -0400

    client AJAX request

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,33 @@
 // Client facing scripts here
+const submitButton = document.querySelector('#submit');
 
-//for use later in making the quizurl
-function generateUrl() {
+$(document).ready(function() {
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
+    console.log("submitting");
+    generateUrl(function(quizURL){
+      $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8080/quiz',
+        data: {quizURL: quizURL },
+        success: function(data) {
+          console.log('Generated URL:', data.quizURL);
+          location.reload();
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.error('Failed to generate URL:', errorThrown);
+        }
+      });
+    });
+  });
+});
+
+function generateUrl(callback) {
   const alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
   let quizUrl = '';
   for (let i = 0; i < 7; i++) {
     const randomUnit = Math.floor(Math.random() * alphabet.length);
-    code += alphabet[randomUnit];
+    quizURL += alphabet[randomUnit];
   }
-  return quizUrl;
+  callback(quizUrl);
 }
-
-module.exports(generateUrl);
