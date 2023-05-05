ad00b88a2553cb5620aa074adf9d0bb3bc710736
Oscar <omankiewicz77@gmail.com>
2023-05-04 22:54:36 -0400

    URL gen made POST ERRORS tho

--- public/scripts/newQuiz.js
+++ public/scripts/newQuiz.js
@@ -1,5 +1,6 @@
-const addNewQuiz = document.querySelector('#submit');
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,8 +15,7 @@ const pool = mysql.createPool({
 
 app.post('/quiz', (req, res) => {
   // Call the createQuizURL function to generate a random URL
-  const quizUrl = generateURL();
-
+  const quizUrl = req.body.quizURL;
   // Insert the URL into the database
   pool.query('INSERT INTO quiz (quizURL) VALUES (?)', [quizUrl], (error, results) => {
     if (error) {
@@ -23,53 +23,16 @@ app.post('/quiz', (req, res) => {
       console.error(error);
       res.status(500).json({ error: 'Failed to save URL' });
     } else {
-      // Retrieve the URL from the database
-      pool.query('SELECT quizURL FROM quiz WHERE quizURL = ?', [quizUrl], (error, results) => {
-        if (error) {
-          // Handle the error if the select operation fails
-          console.error(error);
-          res.status(500).json({ error: 'Failed to retrieve URL' });
-        } else {
-          // Send a response to the client with the generated URL
-          const quizUrl = results[0].quizURL;
-          res.json({ quizUrl: quizUrl });
-        }
-      });
-    }
-  });
-});
-
-
-
-$(document).ready(function() {
-  $("#submit").on("click", function(event) {
-    event.preventDefault();
-    console.log("submitting");
-    location.reload();
+        res.json({ quizUrl: quizUrl });
+      }
   });
 });
 
 console.log("If quiz topic works");
 
-$(document).ready(function(){
-  $("#submit").on("click", function(event){
-    generateUrl();
-  })
-})
-
-function generateUrl() {
-  $.ajax({
-    type: "POST",
-    url: "/quiz",
-    success: function(data) {
-      console.log("Generated URL:", data.quizUrl);
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-      console.error("Failed to generate URL:", errorThrown);
-    }
-  });
-}
-
+app.listen(8080, () => {
+  console.log('Server started on port 8080');
+});
 
 //ill try and implement this later
 /*const addProperty = function(property) {
