da39624fae4ac90a3a5ea79081c77d71ffdfbc18
Oscar <omankiewicz77@gmail.com>
2023-05-05 21:46:42 -0400

    updated client code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,33 +1,19 @@
 // Client facing scripts here
-const submitButton = document.querySelector('#submit');
-
 $(document).ready(function() {
-  $("#submit").on("click", function(event) {
+  $("#quizForm").on("submit", function(event) {
     event.preventDefault();
     console.log("submitting");
-    generateUrl(function(quizURL){
-      $.ajax({
-        type: 'POST',
-        url: 'http://localhost:8080/quiz',
-        data: {quizURL: quizURL },
-        success: function(data) {
-          console.log('Generated URL:', data.quizURL);
-          location.reload();
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-          console.error('Failed to generate URL:', errorThrown);
-        }
-      });
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:8080/quiz',
+      data: { quizNumber: 1 },
+      success: function(data) {
+        console.log('Stored quiz number:', data.quizNumber);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error('Failed to store quiz number:', errorThrown);
+      }
     });
   });
 });
 
-function generateUrl(callback) {
-  const alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
-  let quizUrl = '';
-  for (let i = 0; i < 7; i++) {
-    const randomUnit = Math.floor(Math.random() * alphabet.length);
-    quizURL += alphabet[randomUnit];
-  }
-  callback(quizUrl);
-}
